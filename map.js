f92dca01cdad77dfc25a31d25888e896c5e02b62
vipra-b <88008387+vipra-b@users.noreply.github.com>
2026-05-15 23:18:15 -0700

    api call

--- map.js
+++ map.js
@@ -1,5 +1,15 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-// Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoidmlwcmFiIiwiYSI6ImNtcDd3NmhtYTAyNzEydHB6Y3k2ODYwaHoifQ.ZgLf0aCtCJ0G7aFPrgQcTw';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [32.87943289428407, -117.23224964684451], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
